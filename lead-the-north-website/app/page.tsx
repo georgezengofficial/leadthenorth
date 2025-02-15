fb9ea626b7838b9f4663886da145c5a57dd06744
georgezengofficial <george.zeng.work@gmail.com>
2025-02-15 21:22:10 +0000

    updated page.tsx

--- lead-the-north-website/app/page.tsx
+++ lead-the-north-website/app/page.tsx
@@ -1,7 +1,3 @@
-"use client"; // Needed for Next.js App Router
-
-import React from "react";
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
